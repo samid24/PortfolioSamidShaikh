d58f22324614fc3dc9b6f8395f3dca5448f51e32
samid24 <151507634+samid24@users.noreply.github.com>
2025-11-09 18:51:14 +0530

    Delete Portfolio-Design-Change directory

--- Portfolio-Design-Change/script.js
+++
@@ -1,7 +0,0 @@
-const navBtn = document.getElementById("navBtn");
-const navIteam = document.getElementById("navIteam");
-
-// Mobile Nav
-navBtn.addEventListener("click", function() {
-    navIteam.classList.toggle("hide");
-})
